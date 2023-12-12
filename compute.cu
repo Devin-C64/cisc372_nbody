4df75ba0502c8791ef70325e2fc8db68c5758455
Devin <devinjc@udel.edu>
2023-12-11 21:33:54 -0500

    revert changes

--- compute.cu
+++ compute.cu
@@ -104,17 +104,18 @@ __global__ void pairwise( vector3* d_accels, vector3* d_hPos, double* d_mass){
 
 __global__ void sumrows(vector3* d_accels, vector3* d_hVel, vector3* d_hPos){
 	int i = threadIdx.x + blockIdx.x * blockDim.x;
-	if (i < NUMENTITIES){
+	int j = threadIdx.y + blockIdx.y * blockDim.y;
+	int index = i * NUMENTITIES + j;
+	if (i < NUMENTITIES && j < NUMENTITIES){
 		vector3 accel_sum={0,0,0};
-		for (int j=0;j<NUMENTITIES;j++){
-			for (int k=0;k<3;k++)
-				accel_sum[k]+=d_accels[i * NUMENTITIES + j][k];
-		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
-		for (int k=0;k<3;k++){
-			d_hVel[i][k]+=accel_sum[k]*INTERVAL;
-			d_hPos[i][k]+=d_hVel[i][k]*INTERVAL;
+		for (int k=0;k<3;k++)
+				accel_sum[k]+=d_accels[index][k];
+
+		if (j == 0){
+			for (int k=0;k<3;k++){
+				d_hVel[i][k]+=accel_sum[k]*INTERVAL;
+				d_hPos[i][k]+=d_hVel[i][k]*INTERVAL;
+			}
 		}
 	}
 }
